d1d9f5266faceb211bd67b0a54b68b7b48b4a6ce
barraoc <barraoc@live.co.uk>
2013-03-05 12:39:18 +0000

    Re-commit

---
+++ Sogeti.ProjectAndProposals.BusinessEntity/DomainFacade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sogeti.ProjectsAndProposals.Database;
+using Sogeti.ProjectsAndProposals.DataObjects;
+
+namespace Sogeti.ProjectsAndProposals.BusinessEntity
+{
+    public class DomainFacade
+    {
+        public static DataObjects.Project RetrieveProject(int projectID)
+        {
+            Database.Project prj = new Database.Project();
+            return prj.LoadProject(projectID);
+        }
+
+        public static List<DataObjects.Project> GetProjectList()
+        {
+            return Database.Project.GetAllProjects();
+        }
+
+        public static List<DataObjects.ProjectSummary> GetProjectIDDTOList()
+        {
+            return Database.Project.GetAllProjectIDs();
+        }
+
+    }
+}
---
+++ Sogeti.ProjectsAndProposals.DataObjects/ProjectSummary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sogeti.ProjectsAndProposals.DataObjects
+{
+    public class ProjectSummary
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+
+        public ProjectSummary()
+        {
+        }
+
+        public ProjectSummary(long id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+    }
+}
