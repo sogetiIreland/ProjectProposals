8341dd3fa70a6773f4d338c5ef34800db2c30699
Abdul Muneeb Abbasi <abdul.abbasi@sogeti.com>
2013-03-11 10:38:07 +0000

    connecting to azure db

--- Sogeti.ProjectAndProposals.Database/Generics.cs
+++ Sogeti.ProjectAndProposals.Database/Generics.cs
@@ -14,8 +14,8 @@ namespace Sogeti.ProjectsAndProposals.Database
         {
             get
             {
-                return "Data Source=localhost;Initial Catalog=ProjectsAndProposals;User ID=amabbasi;Password=SogetiIreland;Integrated Security=True";
-                //return "Server=tcp:z59x1q5h4x.database.windows.net,1433;Database=[ProjectsAndProposals];User ID=amabbasi@z59x1q5h4x;Password=SogetiIreland16;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+                //return "Data Source=localhost;Initial Catalog=ProjectsAndProposals;User ID=amabbasi;Password=SogetiIreland;Integrated Security=True";
+                return "Server=tcp:z59x1q5h4x.database.windows.net,1433;Database=ProjectsAndProposals;User Id=amabbasi@z59x1q5h4x;Password=SogetiIreland16;Trusted_Connection=False;Encrypt=False;Connection Timeout=30;";
             }
         }
 
